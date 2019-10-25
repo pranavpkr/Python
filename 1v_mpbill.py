207609d215fdda5f668ee85962e5079b5727aa79
Pranav Pailkar <44741353+pranavpkr@users.noreply.github.com>
2019-10-25 10:32:12 +0530

    Add files via upload

---
+++ 1v_mpbill.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 25 00:21:51 2019
+
+@author: prana
+"""
+from multiprocessing import Process, Lock
+from time import sleep
+from random import random
+from datetime import datetime
+from sys import argv
+import pandas
+
+#Line count = FINDSTR /R /N "^.*$" file.txt | FIND /C ":"
+def work( idofp, chunkf,lck ):
+    with open('mp.txt', 'a') as op:
+        for i,r in chunkf.iterrows():
+            sleep(.1)
+            lck.acquire()
+            op.write( str(i)+"|"+"|".join(r) + "\n" )
+            lck.release()
+
+if __name__ == "__main__": 
+    # creating processes
+
+    noofp = int(argv[1]) if len( argv ) > 1 else 4
+    cz = int(argv[2]) if len( argv ) > 2 else 100
+    print('No of threads',noofp )
+    lock = Lock()
+    processes =[]
+
+    #open file    
+    with open('mp.txt', 'w') as op: pass
+    
+    start = datetime.now()
+    
+    i=0
+    for chunkf in pandas.read_csv('file.dsv', chunksize = cz, sep='|',dtype=str):
+        i+=1
+        print(i)#, end = '\r')
+
+        processes.append( Process(target=work, args=( i, chunkf, lock, ))  )
+
+        if len(processes) > noofp:
+            eval( "[p.start() for p in processes ]")
+            eval( "[p.join() for p in processes ]")
+            processes = []; i=0
+
+    for remaingp in processes:remaingp.start()
+    for remaingp in processes:remaingp.join()
+
+    print( "MP done in", datetime.now()-start )
+    
+    start = datetime.now()
+    df = pandas.read_csv('file.dsv', sep='|',dtype=str)
+    with open('sp.txt', 'w') as op:
+        for i,r in df.iterrows():
+            sleep(.1)
+            print( i, end = '\r')
+            op.write( str(i)+"|"+"|".join(r) + "\n" )
+    print( "SP done in", datetime.now()-start )
+    
